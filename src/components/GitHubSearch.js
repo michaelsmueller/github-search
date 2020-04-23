91179fd3a180cd7ac773678cbc7191f1c75c871c
Michael Mueller <michael@miniguide.co>
2020-04-23 21:58:32 +0200

    add app states and views

--- src/components/GitHubSearch.js
+++ src/components/GitHubSearch.js
@@ -4,27 +4,58 @@ import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import '../App.css';
 
+const STATUS = {
+  LOADING: 'loading',
+  LOADED: 'loaded',
+  ERROR: 'error',
+};
+
 export default class GitHubSearch extends Component {
-  state = { search: '', results: [] };
+  state = {
+    search: '',
+    results: [],
+    error: undefined,
+    status: STATUS.LOADING,
+  };
 
   search(search) {
-    console.log('searching', search);
     axios
       .get(`https://api.github.com/search/users?q=${search}`)
-      .then((response) => this.setState({ results: response.data.items }))
-      .catch((error) => console.log('error', error))
+      .then((response) => {
+        this.setState({
+          results: response.data.items,
+          status: STATUS.LOADED,
+        })
+      })
+      .catch((error) => {
+        console.log('error', error.message);
+        this.setState({
+          error: error.message,
+          status: STATUS.ERROR,
+        })
+      })
   }
 
   setSearch = (search) => this.setState({ search });
 
-  handleClick = (e) => this.search(this.state.search);
+  handleClick = (e) => {
+    this.setState({
+      status: STATUS.LOADING,
+    }, this.search(this.state.search)
+  )}
+  
+  componentDidMount = () => {
+    this.setState({
+      status: STATUS.LOADED,
+    })
+  }
 
   render() {
-    const { search, results } = this.state;
+    const { search } = this.state;
     return (
       <main>
-        <SearchBar search={search} setSearch={this.setSearch} handleClick={this.handleClick} />
-        <SearchResults results={results} />
+        <SearchBar search={search} setSearch={this.setSearch} handleClick={this.handleClick} />        
+        <SearchResults state={this.state} STATUS={STATUS} />
       </main>
     );
   }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import '../App.css';
 
-const SearchResults = ({ results }) => {
-  return results.map((user, index) => <div className='search-result' key={index}>{user.login}</div>);
+const SearchResults = ({ state, STATUS }) => {
+  const { results, status, error } = state;
+  // eslint-disable-next-line default-case
+  switch (status) {
+    case STATUS.LOADING:
+      return <div>Loading</div>;
+    case STATUS.LOADED:
+      return (
+        <ul>
+          {results.map((user, index) => 
+            <li className='search-result' key={index}>
+              <a href={user.html_url}>{user.login}</a>
+            </li>
+          )}
+        </ul>
+      )
+    case STATUS.ERROR:
+      return <div>Error: {error}</div>
+  }
 }
 
 export default SearchResults;
